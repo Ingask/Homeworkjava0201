0a8362b70e872a55e432c066bc398697b4578259
Inga Oberemchuk <inga_oberemchuk@mail.ru>
2020-09-18 15:28:07 +0300

    Files added

---
+++ untitled/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int account = 2_000_000_000;
+        int refill = 500_000_000;
+        int balance = account + refill;
+        System.out.println(balance);
+    }
+}
